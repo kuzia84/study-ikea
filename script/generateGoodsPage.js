988cdcc49248be583362efa7f1d954ab35bc13d7
kuzia84 <kuzia84@gmail.com>
2020-10-03 00:53:18 +0300

    day3 HW

--- script/generateGoodsPage.js
+++ script/generateGoodsPage.js
@@ -1,5 +1,6 @@
 import { getData } from "./getData.js";
-const wishList = ["idd005", "idd100", "idd077", "idd033"];
+const COUNTER = 6;
+const wishList = [];
 const generateGoodsPage = () => {
   const mainHeader = document.querySelector(".main-header");
   const goodsList = document.querySelector(".goods-list");
@@ -9,6 +10,10 @@ const generateGoodsPage = () => {
     const prop = search.split("=")[0].substring(1);
     const value = search.split("=")[1];
     const generateCards = (data) => {
+      if (!data.length) {
+        const goods = document.querySelector('.goods');
+        goods.textContent = location.search === '?wishlist' ? 'Список желаний пуст' : 'По вашему запросу ничего не найдено';
+      }
       data.forEach((item) => {
         goodsList.insertAdjacentHTML(
           "afterbegin",
@@ -17,30 +22,30 @@ const generateGoodsPage = () => {
             <a class="goods-item__link" href="card.html#${item.id}">
                 <article class="goods-item">
                     <div class="goods-item__img">
-                        <img src="${item.img[0]}"
-                            data-second-image="${item.img[1]}"
-                            alt="${item.name}">
-                    </div>
-                    <p class="goods-item__new"><!--Новинка--></p>
+                        <img src=${item.img[0]}
+                            ${item.img[1] ? `data-second-image=${item.img[1]}` : ''}
+                            alt = "${item.name}" >
+                    </div >
+                    ${item.count > COUNTER ? `<p class="goods-item__new">Новинка</p>` : ''}
+                    ${!item.count ? `<p class="goods-item__new">Нет в наличаи</p>` : ''}
                     <h3 class="goods-item__header">${item.name}</h3>
                     <p class="goods-item__description">${item.description}</p>
                     <p class="goods-item__price">
                         <span class="goods-item__price-value">${item.price}</span>
                         <span class="goods-item__currency"> ₽</span>
                     </p>
-                    <button class="btn btn-add-card" aria-label="Добравить в корзину"
-                        data-idd="${item.id}"></button>
-                </article>
-            </a>
-          </li>
-          `
+                    ${item.count ? `<button class="btn btn-add-card" aria-label="Добравить в корзину" data-idd="${item.id}"></button>` : ''}
+                </article >
+            </a >
+          </li >
+  `
         );
       });
     };
     if (prop === "s") {
       getData.search(value, generateCards);
       if (goodsList) {
-        mainHeader.textContent = `Поиск: ${value}`;
+        mainHeader.textContent = `Поиск: ${value} `;
       } else {
         mainHeader.textContent = "Ничего не найдено";
       }
